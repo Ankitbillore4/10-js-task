20bb97d604d81ba7aea740aa22046b809e33b79f
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-07-28 22:36:18 +0530

    formate

--- script2.js
+++ script2.js
@@ -1,46 +1,46 @@
 
 
- var isName= document.querySelector(".Name")
- var Email= document.querySelector(".Email")
- var Password= document.querySelector(".Password")
- var Submit= document.querySelector(".Submit")
- var nameError= document.querySelector(".nameError")
- var EmailError= document.querySelector(".EmailError")
- var PasswordError= document.querySelector(".PasswordError")
- var form= document.querySelector(".form")
+var isName = document.querySelector(".Name")
+var Email = document.querySelector(".Email")
+var Password = document.querySelector(".Password")
+var Submit = document.querySelector(".Submit")
+var nameError = document.querySelector(".nameError")
+var EmailError = document.querySelector(".EmailError")
+var PasswordError = document.querySelector(".PasswordError")
+var form = document.querySelector(".form")
 
- function formValidation(){
-    form.addEventListener("submit",function(e){
+function formValidation() {
+    form.addEventListener("submit", function (e) {
 
-        var emailCheck= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
-       
-        if(isName.value==="" || isName.value==null){
-    
+        var emailCheck = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+
+        if (isName.value === "" || isName.value == null) {
+
             e.preventDefault();
-            nameError.innerHTML="Name Can't be blank "
-        }else{
-            nameError.innerHTML=""
+            nameError.innerHTML = "Name Can't be blank "
+        } else {
+            nameError.innerHTML = ""
         }
-        if(!Email.value.match(emailCheck)){
-        e.preventDefault();
-            EmailError.innerHTML="Valid Email is Required "
-        }else{
-            EmailError.innerHTML=" "
-    
-        }
-    
-        if(Password.value.length <=5){
+        if (!Email.value.match(emailCheck)) {
             e.preventDefault();
-                PasswordError.innerHTML="Password must be more than 6 characters "
-            }else{
-                PasswordError.innerHTML=""
-                
-            }
-     })    
- }
- formValidation()
+            EmailError.innerHTML = "Valid Email is Required "
+        } else {
+            EmailError.innerHTML = " "
 
- 
+        }
+
+        if (Password.value.length <= 5) {
+            e.preventDefault();
+            PasswordError.innerHTML = "Password must be more than 6 characters "
+        } else {
+            PasswordError.innerHTML = ""
+
+        }
+    })
+}
+formValidation()
+
+
 
 
 
