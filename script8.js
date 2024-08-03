6bef9c79903c07dc323adda94b03c117574c41e1
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-08-03 23:36:54 +0530

    changes

--- script8.js
+++ script8.js
@@ -1,11 +1,11 @@
 var nav = document.querySelector(".nav")
 var body = document.querySelector("body")
 
-body.addEventListener("wheel",function(dets){
-    if(dets.deltaY < 0){
-     nav.classList.add("fixed")
-      console.log("ulta");
-    }else{
+body.addEventListener("wheel", function (dets) {
+    if (dets.deltaY < 0) {
+        nav.classList.add("fixed")
+        console.log("ulta");
+    } else {
         nav.classList.remove("fixed")
         console.log("sidda");
     }
