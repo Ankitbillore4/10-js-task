6331da3c45f193bdb418d99b6d43d73a20edb7f3
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-08-03 23:40:22 +0530

    changes

--- script1.js
+++ script1.js
@@ -4,54 +4,54 @@ let slider = document.querySelector(".slider")
 let images = document.querySelectorAll(".images")
 
 let newNumber = 1;
-let imglength =  images.length;
+let imglength = images.length;
 
-let sliding = function(){
-   slider.style.transform=`translateX(-${newNumber*100}%)`
+let sliding = function () {
+   slider.style.transform = `translateX(-${newNumber * 100}%)`
    newNumber++;
 };
-let revsliding = function(){
-   slider.style.transform=`translateX(-${(newNumber-2)*100}%)`
+let revsliding = function () {
+   slider.style.transform = `translateX(-${(newNumber - 2) * 100}%)`
    newNumber--;
 };
 
-let firstslide = function(){
-   slider.style.transform=`translateX(0%)`
-  newNumber= 1;
+let firstslide = function () {
+   slider.style.transform = `translateX(0%)`
+   newNumber = 1;
 }
-let lastslide = function(){
-   slider.style.transform=`translateX(-${(imglength-1)*100}%)`
-  newNumber = imglength;
+let lastslide = function () {
+   slider.style.transform = `translateX(-${(imglength - 1) * 100}%)`
+   newNumber = imglength;
 }
 
-rightarrow.addEventListener("click",function(){
-  if(newNumber < imglength){
-   sliding()
-  }else{
-   firstslide()
-  }
+rightarrow.addEventListener("click", function () {
+   if (newNumber < imglength) {
+      sliding()
+   } else {
+      firstslide()
+   }
 })
 
-leftarrow.addEventListener("click",function(){
+leftarrow.addEventListener("click", function () {
    console.log("helo");
-   if(newNumber > 1){
-    revsliding()
-   }else{
-    lastslide()
+   if (newNumber > 1) {
+      revsliding()
+   } else {
+      lastslide()
    }
- })
+})
 
 
- var automic = setInterval(function() {
-      if(newNumber < imglength){
-         sliding()
-        }else{
-         firstslide()
-        }
-   },2000);
- 
+var automic = setInterval(function () {
+   if (newNumber < imglength) {
+      sliding()
+   } else {
+      firstslide()
+   }
+}, 2000);
 
-slider.addEventListener("mouseenter",function(){
+
+slider.addEventListener("mouseenter", function () {
    clearInterval(automic)
    console.log("helo");
 })
